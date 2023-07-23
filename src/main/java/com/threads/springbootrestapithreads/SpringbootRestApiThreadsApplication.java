4588a352db1ca54452bad8b59ed14ca05ec30781
Giorgi <giogurabanidze@outlook.com>
2023-07-22 19:57:02 -0700

    Initial commit

---
+++ src/main/java/com/threads/springbootrestapithreads/SpringbootRestApiThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.threads.springbootrestapithreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApiThreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threads/springbootrestapithreads/SpringbootRestApiThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threads.springbootrestapithreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
